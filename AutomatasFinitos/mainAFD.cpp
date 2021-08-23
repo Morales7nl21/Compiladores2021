f9519646bc69e288c3dbd176da92acc91ae91960
Morales7nl21 <thedjdave98@gmail.com>
2021-08-22 21:31:58 -0500

    Se agrego las instrucciones de uso en el programa afd

--- AutomatasFinitos/mainAFD.cpp
+++ AutomatasFinitos/mainAFD.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 #include "clases.hpp"
 
+#ifdef _WIN32
+  #include<windows.h>
+#endif  
+
+void limpiar_pantalla()
+{
+  #ifdef _WIN32
+    system("cls");
+  #else
+    system("clear");
+  #endif
+}
+
 vector<char> alfabeto;    
 AFD *a;
 vector<Estado> edos;
@@ -11,6 +24,7 @@ FuncionTransicion *ft;
 
 
 void menu();
+void mostrarUso();
 void ingresarAutomata();
 void verificarPalabra();
 void imprimeTransiciones();
@@ -27,6 +41,7 @@ void menu(){
     do
     {      
         cout << endl;          
+        cout << "0 -> Modo de uso" << endl;
         cout << "1 -> Ingresar Automata" << endl;
         cout << "2 -> Imprimir tabla de transiciones del Automata" << endl;
         cout << "3 -> Verificar Palabra" << endl;
@@ -35,13 +50,29 @@ void menu(){
         cin>>k;
         switch (k)
         {
-        case 1: ingresarAutomata();break;
-        case 2: imprimeTransiciones(); break;        
-        case 3: verificarPalabra(); break;
-        case 4: cout << "Saliendo" << endl; break;     
-        default: cout << "Opcion no valida" << endl; break;
+            case 0: mostrarUso(); break;
+            case 1: ingresarAutomata();break;
+            case 2: imprimeTransiciones(); break;        
+            case 3: verificarPalabra(); break;
+            case 4: cout << "Saliendo" << endl; break;     
+            default: cout << "Opcion no valida" << endl; break;
         }
     } while (k!=4);   
+}
+void mostrarUso(){
+        limpiar_pantalla();
+        cout << "1.- Para hacer uso del programa primero ingrese un automata finito determinista antes de buscar o imprimir la tabla de transiciones" << endl;
+        cout << "2.- Este automata no acepta como entradas numeros en el alfabeto o nombre de los estados" << endl;
+        cout << "3.- De ser que quiera meter un numero representativo en su  alfabeto o como nombre de estado escribalo de la forma: " << endl;
+        cout << " \t\t n0, n1, n2, ..., etc. " << endl;
+        cout << "4.- Se recomienda que para el nombre de los estados estos inicien con 'q', seguido del numero del estado iniciando en el 0. " << endl;
+        cout << "De la forma: \n\t\t q0, q1, q2, ...... , qn " << endl;
+        cout << "5.- Al seleccionar el estado inicial o final ingrese solo el numero al que corresponde su estado" << endl; 
+        cout << "\n\tsi es el primero use el 0 para denotarlo o si es el ultimo use el valor del total de estados menos 1 " << endl;
+        cout << "6.- Para verificar la palabra utilize solo letras o cadenas, no use solo numeros, de ser asi concatenelos con algun char como se menciona en el punto 2 y 3" << endl;            
+        cout << "\nPara continuar introduzca cualquier caracter y presione enter" << endl;
+        char k; cin >> k;
+        limpiar_pantalla();
 }
 void ingresarAutomata(){
     
@@ -82,7 +113,10 @@ void ingresarAutomata(){
         op++;                        
     }
     a = new AFD(alfabeto,edos,inicial,final);
-    t = new Transicion(*a);    
+    t = new Transicion(*a);   
+    cout << "\nPara continuar introduzca cualquier caracter y presione enter" << endl;
+    char k; cin >> k;
+    limpiar_pantalla();
 }
 
 void verificarPalabra(){
@@ -94,10 +128,16 @@ void verificarPalabra(){
     string ini = inicial.getNombreEdo();
     ft = new FuncionTransicion(*a,ini,*t);
     ft->hacerSigTransicion(palIdentificar);
+    cout << "\nPara continuar introduzca cualquier caracter y presione enter" << endl;
+    char k; cin >> k;
+    limpiar_pantalla();
 }
 void imprimeTransiciones(){
     for(auto &&var : t->getTransiciones())
     {
         cout << "["<<var.first.first << ","<< var.first.second << "] : " << var.second << endl; 
     }
+    cout << "\nPara continuar introduzca cualquier caracter y presione enter" << endl;
+    char k; cin >> k;
+    limpiar_pantalla();
 }

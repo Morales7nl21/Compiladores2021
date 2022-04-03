6a547dd758eb0ddeed90a2d3e392105320ab9f54
Morales7nl21 <d.moralesblasescom@gmail.com>
2022-04-02 20:50:19 -0600

    Se comienzan agregando las palabras reservadas

---
+++ Practica3/analizadorLexico.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+vector<string> palReservadas = {"repeat", "Init", "End", "Add", "+", "-", "*", "/", "notTrue", "True", "False"};
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}

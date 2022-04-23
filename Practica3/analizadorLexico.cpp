6668eee3c76c53ee756366fa61c1bc34a9c0518d
Morales7nl21 <d.moralesblasescom@gmail.com>
2022-04-22 19:36:37 -0500

    Se ocupan regex para evaluar que tipo de string es

--- Practica3/analizadorLexico.cpp
+++ Practica3/analizadorLexico.cpp
@@ -3,22 +3,71 @@
 #include "analizadorLexico.hpp"
 using namespace std;
 
-void AnalizadorLexico::examinaArchivo(){
-    cout << "Abriendo archivo a examinar" << nombreArchivo << endl;
+string AnalizadorLexico::examinaArchivo()
+{
+    cout << "Abriendo archivo: " << nombreArchivo << endl;
     ifstream archivo(nombreArchivo); // se lee el archivo
     string linea;
+    string archivoSinEspacios1Linea;
     if (archivo.is_open())
         cout << "Todo bien al abrir archivo" << endl;
-   
-   
-    while(getline(archivo,linea)){
-         
-        string::iterator new_end = unique(linea.begin(), linea.end(), ambosEspacios);
-        linea.erase(new_end, linea.end());   
-        cout << linea << endl;
+    while (getline(archivo, linea))
+    {
+        linea = regex_replace(linea, regex("[' ']{2,}"), " "); // se remplazan mas de 2 spacios por uno con regex
+        archivoSinEspacios1Linea.append(linea);
     }
-
+    return archivoSinEspacios1Linea;
     archivo.close();
-
 }
-bool ambosEspacios(char lhs, char rhs) { return (lhs == rhs) && (lhs == ' '); }
+bool AnalizadorLexico::analizaArchivo(string cad)
+{
+
+    string aEvaluar = "";
+    regex regexA("[A-Z]+");
+    regex regexB("[a-z]+");
+    regex regexC("[0-9]+");
+    regex regexD("['/'|'*'|'+'|'-'|'('|')'|]+");
+    regex regexE("\\s+");
+    regex regexF("(_)+");
+    bool palR = false;
+    for (auto const &c : cad)
+    {
+        if (c == ' ' || c==';' || c==':')
+        {
+            if(aEvaluar.size()>0){
+
+                 if(regex_match(aEvaluar,regexA))
+                     cout << aEvaluar<< "->" << "is a A" << endl;
+                 else if(regex_match(aEvaluar,regexB))
+                     cout << aEvaluar<< "->" << "is a B" << endl;
+                 else if(regex_match(aEvaluar,regexC))
+                     cout << aEvaluar<< "->" << "is a C" << endl;
+                 else if(regex_match(aEvaluar,regexD))
+                     cout << aEvaluar<< "->" << "is a D" << endl;
+                 else if(regex_match(aEvaluar,regexE))
+                     cout << aEvaluar<< "->" << "is a E" << endl;
+                 else if(regex_match(aEvaluar,regexF))
+                     cout << aEvaluar<< "->" << "is a F" << endl;    
+                
+                else   {
+                    for (auto const & pr : palReservadas)
+                    {
+                        if(aEvaluar == pr){
+                            cout << aEvaluar<< "->" << "is a Pal Reservada" << endl;    
+                            palR = true;
+                            break;
+                        } 
+                    }                    
+                    if(!palR)
+                        cout << aEvaluar <<"-> Its nothing :0" << endl;
+                } 
+
+            }
+            aEvaluar.clear();            
+        }
+        else
+            aEvaluar.push_back(c);
+    }    
+       
+    return false;
+}
--- Practica3/analizadorLexico.hpp
+++ Practica3/analizadorLexico.hpp
@@ -4,15 +4,25 @@
 #include "../Practica1/clases.hpp"
 using namespace std;
 
-class AnalizadorLexico{
-    private:
-        AFD* afd;
-        AFN* afn; 
-        bool resultadoAnalisis;
-        string nombreArchivo;
-    public:
-        friend bool ambosEspacios(char, char);   
-        AnalizadorLexico(string nombreArchivo):nombreArchivo(nombreArchivo){};
-        void examinaArchivo();
-        void analizaArchivo();
+class AnalizadorLexico
+{
+private:
+    AFD *afd;
+    AFN *afn;
+    bool resultadoAnalisis;
+    string nombreArchivo;
+    vector<string> palReservadas = {"repeat", "Init", "End", "Add",
+                                    "notTrue", "True", "False", "declare",
+                                    "AsNumber", "AsFloat", "AsChar", "Array", "len", "AsString",
+                                    "()", "End", "showNumber", "showFloat", "showChar", "showString", 
+                                    "value", "Zero", "void", "number", "char", "string"};
+
+    vector<string> D = {"/", "*", "+", "-", "{", "}", "(", ")", "<", ">", ".", ":", ">>", "<-", "->"};
+    vector<char> E = {' '};
+    char F = '_';
+
+public:
+    AnalizadorLexico(string nombreArchivo) : nombreArchivo(nombreArchivo){};
+    string examinaArchivo();
+    bool analizaArchivo(string);
 };

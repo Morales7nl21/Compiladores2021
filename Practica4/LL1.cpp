0a7bf157d709b23e74aada3ddea47a2270f147e1
Morales7nl21 <d.moralesblasescom@gmail.com>
2022-05-29 02:06:05 -0500

    Se crean los valores primeros

--- Practica4/LL1.cpp
+++ Practica4/LL1.cpp
@@ -1,120 +1,209 @@
 #include <bits/stdc++.h>
 #include <iostream>
-#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <iterator>
+#include <sstream>
 #include "LL1.hpp"
 
-vector<string> LL1::primeros(string simbolo){
-    vector<string> conjuntoRetorno;
-    //Condiciones dadas de la profesora.
-    if(verificaSiEsTerminal(simbolo)){ //el simbolo es terminal
-        conjuntoRetorno.push_back(simbolo);
-    }else if(simbolo == "ε"){
-        bool band_existeE = false;
-        for (auto const &cR : conjuntoRetorno)
-        {
-            if(cR=="ε"){
-                band_existeE = true;
+void LL1::primeros(const vector<string> &elementoAbuscarPrimero, vector<string> &conjuntoRetorno)
+{
+
+    for (auto const &eAP : elementoAbuscarPrimero)
+    {
+        if (!verificaSiEsNoTerminal(eAP))
+        { // el simbolo es terminal
+            bool band_existeE = false;
+            for (auto const &cR : conjuntoRetorno)
+            {
+                if (cR == eAP)
+                {
+                    band_existeE = true;
+                }
+            }
+            if (!band_existeE)
+            {
+                conjuntoRetorno.push_back(eAP);
             }
         }
-        if(!band_existeE){
-            conjuntoRetorno.push_back("ε");
+        else if (eAP == "ε")
+        {
+            bool band_existeE = false;
+            for (auto const &cR : conjuntoRetorno)
+            {
+                if (cR == "ε")
+                {
+                    band_existeE = true;
+                }
+            }
+            if (!band_existeE)
+            {
+                conjuntoRetorno.push_back("ε");
+            }
+        }
+        else if (verificaSiEsNoTerminal(eAP))
+        {
+            for (auto const &pNC : primerosNoConjunto)
+            {                
+                if(eAP == pNC.first ){
+                    primeros(pNC.second, conjuntoRetorno);
+                }
+            }            
         }
         
-    }else if(!verificaSiEsTerminal(simbolo)){
-        vector<string> conjuntoSimbolosPrimeros {};      
-        conjuntoSimbolosPrimeros = verificaPrimero(simbolo);
-        if(!conjuntoSimbolosPrimeros.empty()){
-            for (auto const &cSP : conjuntoSimbolosPrimeros)
-            {
-                conjuntoRetorno.push_back(cSP);
-            }            
-        }else{
-            //ESTA MAL
-            conjunto_primeros[simbolo] = {simbolo};
-        }
     }
-    return conjuntoRetorno;
 }
-bool LL1::verificaSiEsTerminal(string simbolo){
+bool LL1::verificaSiEsNoTerminal(string simbolo)
+{
     bool esTerminal = false;
-    for (auto const &t : terminales)
+    for (auto const &t : noTerminales)
     {
-        if(t==simbolo){
-            esTerminal=true;
+        if (t == simbolo)
+        {
+            esTerminal = true;
             break;
         }
     }
     return esTerminal;
 }
-vector<string> LL1::verificaPrimero(string simbolo){    
-    for (auto  p : conjunto_primeros)
-    {        
-        if(p.first == simbolo && !p.second.empty()){
-            return p.second;                        
+vector<string> LL1::verificaPrimero(string simbolo)
+{
+    for (auto p : conjunto_primeros)
+    {
+        if (p.first == simbolo && !p.second.empty())
+        {
+            return p.second;
         }
-    }  
+    }
     vector<string> r{};
-    return r;  
+    return r;
 
-}/*
-vector<string> gramatica = {
-        "E ⟶ TE’",
-        "E’ ⟶ + TE’ | ε",
-        "T ⟶ FT’",
-        "T’ ⟶ * FT’ | ε",
-        "F ⟶ id | ( E )"};
-*/
-void LL1::asignaTermNoTermYProduc(){
-    
-    vector<vector<string>> v_gramatica{};
+} /*
+ vector<string> gramatica = {
+         "E ⟶ T E’",
+         "E’ ⟶ + T E’ | ε",
+         "T ⟶ FT’",
+         "T’ ⟶ * FT’ | ε",
+         "F ⟶ id | ( E )"};
+ */
+void LL1::asignaTermNoTermYProduc()
+{
+
     for (auto const &simbolo : gramatica)
     {
         stringstream ss(simbolo);
         istream_iterator<std::string> begin(ss);
         istream_iterator<std::string> end;
-        vector<std::string> vector_gramatica(begin, end);    
-        terminales.push_back(vector_gramatica[0]);                  
-        v_gramatica.push_back(vector_gramatica);       
+        vector<std::string> vector_gramatica(begin, end);
+        noTerminales.push_back(vector_gramatica[0]);
+        v_gramatica.push_back(vector_gramatica);
     }
-    //Despues de que sabemos cuales son los no temrinales recorremos cada vector de la gramatica para sacar lso terminales
+    // Despues de que sabemos cuales son los no temrinales recorremos cada vector de la gramatica para sacar lso terminales
     bool banderaTerminalEncontrado = false;
     for (auto const &vector : v_gramatica)
     {
         for (auto const &string_simbolo : vector)
-        {            
-            if(!verificaSiEsTerminal(string_simbolo)){
-                
-                if(string_simbolo!="⟶" && string_simbolo != "|"){
-                    
+        {
+            if (!verificaSiEsNoTerminal(string_simbolo))
+            {
+
+                if (string_simbolo != "->" && string_simbolo != "|")
+                {
+
                     for (auto const &nt : noTerminales)
                     {
-                        if(string_simbolo == nt){
+                        if (string_simbolo == nt)
+                        {
                             banderaTerminalEncontrado = true;
                             break;
-                        }        
+                        }
                     }
-                    if(!banderaTerminalEncontrado){
-                        noTerminales.push_back(string_simbolo);
+                    if (!banderaTerminalEncontrado)
+                    {
+                        terminales.push_back(string_simbolo);
                     }
                     banderaTerminalEncontrado = false;
                 }
+            }
+        }
+    }
+}
 
-                    
-            }                        
-        }                
-    }
-    cout << "NO TERMINALEs " << endl;
-    for (auto const &nt : noTerminales)
+void LL1::obtienePrimeros()
+{
+    int cont = 0;
+    vector<string> r{};
+    
+    for (auto const &t : noTerminales)
     {
-        cout << nt << endl;
+
+        ostringstream vts;
+        copy(v_gramatica[cont].begin(), v_gramatica[cont].end() - 1, ostream_iterator<string>(vts, " "));
+        vts << v_gramatica[cont].back(); // se agrega ultimo elemento sin identificador ','
+        string vts_s = vts.str();        // contiene el elemento que va despues del simbolo de produce "->"
+        // rightTrim(vts_s);
+        vector<string> delimitadores{"->", " "};
+        vector<string> sentencias{}; // contiene todas las producciones despues de or '|'
+
+        leftTrim(vts_s, delimitadores[0]); // Cortamos la cadena hasta -> por izquierda
+        sentencias = split(vts_s, '|');
+
+        // rightTrim(vts_s, delimitadores[1]); //
+        for (auto &&i : sentencias)
+        {
+            // LIMPIAMOS DATA pa dejar solo los primeros de los conjuntos
+            if (i[0] == ' ')
+                i = i.substr(1, i.size());
+            if (i.size() > 2)
+                rightTrim(i, delimitadores[1]); // cortamos la cadena despues del priemr space para obtener solo el primer elemnto
+            i.erase(remove(i.begin(), i.end(), ' '), i.end());
+            // cout << "S " << i  << " z: "<< i.length()<< endl;
+        }
+
+        primerosNoConjunto.push_back(make_pair(t, sentencias)); // Se añade al grupo de pares de primeros
+        cont++;
     }
-    cout << "TERMINALEs " << endl;
+
+    cout << "NO Terminales" << endl;
+    for (auto const &t : noTerminales)
+    {
+        cout << t << endl;
+    }
+    cout << "Terminales" << endl;
     for (auto const &t : terminales)
     {
         cout << t << endl;
     }
-    cout << endl;
+    cout << "Pares primero" << endl;
+
+    for (auto const &pnc : primerosNoConjunto)
+    {
+        cout << pnc.first << ": ";
+        for (auto const &conjP : pnc.second)
+        {
+            cout << conjP << ",";
+        }
+        cout << endl;
+    }
     
     
+    for (auto const &pnc : primerosNoConjunto)
+    {
+        vector<string> cr{};
+        cout << "ENCONTRANDO PRIMEROS" << endl;
+        
+        cout << pnc.first << ": ";
+        primeros(pnc.second, cr);
+        
+        for (auto const &i : cr)
+        {
+             cout << i << endl;
+        }
+        
+    }
+    
+    cout << endl
+         << endl;
+
 
 }
--- Practica4/LL1.hpp
+++ Practica4/LL1.hpp
@@ -6,36 +6,70 @@ class LL1
 {
 private:
     vector<string> gramatica = {
-        "E ⟶ T E’",
-        "E’ ⟶ + T E’ | ε",
-        "T ⟶ F T’",
-        "T’ ⟶ * F T’ | ε",
-        "F ⟶ id | ( E )"};
+        "E -> T E’",
+        "E’ -> + T E’ | ε",
+        "T -> F T’",
+        "T’ -> * F T’ | ε",
+        "F -> id | ( E )"};
     vector<vector<string>> tabla;
     map<string, vector<string>> conjunto_primeros;
     vector<vector<string>> conjunto_segundos;
     vector<string> terminales;
     vector<string> noTerminales;
+    vector<vector<string>> v_gramatica{};
+    vector<pair<string, vector<string>>> primerosNoConjunto;
 
 public:
     LL1()
     {
         asignaTermNoTermYProduc();
+        obtienePrimeros();
     }
+    void obtienePrimeros();
     void asignaTermNoTermYProduc();
-    vector<string> primeros(string);
+    /*
+        El primer parametro hace referencia al simbolo del cual se busca su primero
+        el segundo parametro hace referencia al primer elemento despues del simbolo '->'
+        el tercer parametro hace referencia al conjunto que guardará sus primeros
+    */
+    void primeros( const vector<string> &, vector<string> &);
     vector<string> siguientes(string);
     void ConstruirTabla();
     void ProbarCadena(string);
     void GetTerminales();
     void GetnoTerminales();
     vector<string> verificaPrimero(string);
-    bool verificaSiEsTerminal(string);
+    bool verificaSiEsNoTerminal(string);
+    
     void imprimeGramatica()
     {
         for (auto const &g : gramatica)
         {
             cout << g << endl;
         }
+    }
+    string &leftTrim(string &str, string const &chars)
+    {
+        str.erase(0, str.find_first_of(chars) + 3); // Cortamos el elemento '-' '>' y space por eso '3'
+        return str;
+    }
+    string &rightTrim(string &str, string const &chars)
+    {
+        str.erase(str.find_first_of(chars)+1);
+        return str;
+    }
+    vector<string> split(const string &s, char seperator)
+    {
+        vector<std::string> output;
+        string::size_type prev_pos = 0, pos = 0;
+
+        while ((pos = s.find(seperator, pos)) != string::npos)
+        {
+            string substring(s.substr(prev_pos, pos - prev_pos));
+            output.push_back(substring);
+            prev_pos = ++pos;
+        }
+        output.push_back(s.substr(prev_pos, pos - prev_pos)); // Last word
+        return output;
     }
 };

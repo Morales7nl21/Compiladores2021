67840ce2c16475cee12331be1bfcca17d620d836
Morales7nl21 <d.moralesblasescom@gmail.com>
2022-06-10 22:35:06 -0500

    Se añade el nombre al main de errores semanticas

--- Proyecto/AnalizadorLexico/main.cpp
+++ Proyecto/AnalizadorLexico/main.cpp
@@ -7,9 +7,10 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    //AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizar.txt"); //Comenzamos a analizar lexicamente
+    //AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizar.txt"); //Comenzamos a analizar lexicamente    
     //AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizarErroresLexicos.txt");
-    AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizarErroresSintacticos.txt");
+    //AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizarErroresSintacticos.txt");
+    AnalizadorLexico *al = new AnalizadorLexico("archivoAnalizarErroresSemanticos.txt"); //Comenzamos a analizar lexicamente
     string alEA = al->examinaArchivo();    
     cout << alEA << endl;
     al->analizaArchivo(alEA);
